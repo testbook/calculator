9665030e3b3de20c1b31be9804892088feeb807a
sumantestbook <suman@testbook.com>
2022-06-06 18:45:58 +0530

    added more calc

--- script/tb-calc-type-1.js
+++ script/tb-calc-type-1.js
@@ -19,48 +19,76 @@ const errorAlertSelector = document.getElementById('errorAlert');
 
 
 window.onload = function() {
+    
+    calcTitleTxt.innerText = `${calcType.substring(0, calcType.search(2))} to ${calcType.substring(calcType.search(2)+1)} Calculator`;
+    calcInputText.innerText = `${calcType.substring(0, calcType.search(2))}`;
+    calcInputAnsText.innerText = `${calcType.substring(calcType.search(2)+1)}`;
+
     switch (calcType) {
+
         case "billion2rupees":
-        calcTitleTxt.innerText = 'Billion into Rupees Calculator';
-        calcInputText.innerText = 'Billion';
-        calcInputAnsText.innerText = 'Rupees';
         calcHelpherText.innerText = 'Ex: 1 Billion = 1000000000 Rupees';
         break;
-        case "feet2cm":
-        calcTitleTxt.innerText = 'Feet to Centimeter Calculator';
-        calcInputText.innerText = 'Feet';
-        calcInputAnsText.innerText = 'CM';
+        case "million2rupees":
+        calcHelpherText.innerText = 'Ex: 1 Million = 1000000 Rupees';
+        break;
+        case "feet2centimeter":
         calcHelpherText.innerText = 'Ex: 1 = 30.48cm';
         break;  
         case "gallon2liter":
-        calcTitleTxt.innerText = 'Gallon to Liter Calculator';
-        calcInputText.innerText = 'Gallon';
-        calcInputAnsText.innerText = 'Liter';
         calcHelpherText.innerText = 'Ex: 1 Gallon = 3.785 Liter';
         break;
-        case "mm2cm":
-        calcTitleTxt.innerText = 'mm to cm Calculator';
-        calcInputText.innerText = 'mm';
-        calcInputAnsText.innerText = 'cm';
+        case "millimeter2centimeter":
         calcHelpherText.innerText = 'Ex: 1mm = 0.1 cm';
         break;
         case "million2lakhs":
-        calcTitleTxt.innerText = 'Million to Lakhs Calculator';
-        calcInputText.innerText = 'Million';
-        calcInputAnsText.innerText = 'Lakhs';
         calcHelpherText.innerText = 'Ex: 1 Million = 10 Lakhs';
         break;
-        case "metres2feet":
-        calcTitleTxt.innerText = 'Meter To Feet Calculator';
-        calcInputText.innerText = 'Meter';
-        calcInputAnsText.innerText = 'Feet';
+        case "metre2feet":
         calcHelpherText.innerText = 'Ex: 1 Meter = 3.281 Feet';
         break;
         case "lbs2kg":
-        calcTitleTxt.innerText = 'lbs to kg Calculator';
-        calcInputText.innerText = 'lbs';
-        calcInputAnsText.innerText = 'kg';
         calcHelpherText.innerText = 'Ex: 1 lbs = 0.45359 Kg';
+        break;
+        case "million2crore":
+        calcHelpherText.innerText = 'Ex: 1 Million = .1 Crore';
+        break;
+        case "meter2centimeter":
+        calcHelpherText.innerText = 'Ex: 1 Meter = 100 CM';
+        break;
+        case "centimeter2meter":
+        calcHelpherText.innerText = 'Ex: 1 Cm = 0.01';
+        break;
+        case "centimeter2millimeter":
+        calcHelpherText.innerText = 'Ex: 1 Centimeter = 10 Millimeter';
+        break;
+        case "centimeter2foot":
+        calcHelpherText.innerText = 'Ex: 1 Centimeter = 0.033 Foot';
+        break;
+        case "centimeter2kilometer":
+        calcHelpherText.innerText = 'Ex: 1 Centimeter = 0.00001 KiloMeter';
+        break;
+        case "inch2feet":
+        calcHelpherText.innerText = 'Ex: 1 Inch = 0.083 Feet';
+        break;
+        case "meter2millimeter":
+        calcHelpherText.innerText = 'Ex: 1 Meter = 1000 Meter';
+        break;
+        case "meter2kilometer":
+        calcHelpherText.innerText = 'Ex: 1 Meter = 0.001 KiloMeter';
+        break;
+        case "meter2inch":
+        calcHelpherText.innerText = 'Ex: 1 Meter = 39.370 Inch';
+        break;
+        case "kilometer2mile":
+        calcHelpherText.innerText = 'Ex: 1 Kilometer = 6.214 Mile';
+        break;
+        case "cubicfeet2cubicmeter":
+        calcTitleTxt.innerText = 'Cubic Foot To Cubic Meter Calculator'
+        calcHelpherText.innerText = 'Ex: 1 Cubic Foot = 0.028 Cubic Meter';
+        break;
+        case "hour2minute":
+        calcHelpherText.innerText = 'Ex: 1 Hour = 60 Minutes';
         break;
         default:
         calcTitleTxt.innerText = 'wrong url';
@@ -78,12 +106,17 @@ function calculateFun() {
     if(!inputSelector.checkValidity() || inputSelector.validity.patternMismatch) {
         inputSelector.parentElement.classList.add('has-error');
         resultSelector.value = "";
+        calcAnsText.innerText = "";
     }
     else if(calcType === 'billion2rupees'){
      resultSelector.value = Math.round(Number(inputSelector.value)*1000*1000000* 1000) / 1000;
      calcAnsText.innerText = userHelpText(resultSelector.value);
     } 
-    else if(calcType === 'feet2cm'){
+    else if(calcType === 'million2rupees'){
+     resultSelector.value = Math.round(Number(inputSelector.value)*1000000* 1000) / 1000;
+     calcAnsText.innerText = userHelpText(resultSelector.value);
+    } 
+    else if(calcType === 'feet2centimeter'){
         resultSelector.value = Math.round(Number(inputSelector.value)*30.48 * 100) / 100;
         calcAnsText.innerText = userHelpText(resultSelector.value);
     }
@@ -91,7 +124,7 @@ function calculateFun() {
         resultSelector.value = Math.round(Number(inputSelector.value)*3.78541* 1000) / 1000;
         calcAnsText.innerText = userHelpText(resultSelector.value);
     }
-    else if(calcType === 'mm2cm'){
+    else if(calcType === 'millimeter2centimeter'){
         resultSelector.value = Math.round(Number(inputSelector.value)*0.1* 100) / 100;
         calcAnsText.innerText = userHelpText(resultSelector.value);
     }
@@ -99,12 +132,64 @@ function calculateFun() {
         resultSelector.value = Math.round(Number(inputSelector.value)*10* 100) / 100;
         calcAnsText.innerText = userHelpText(resultSelector.value);
     }
-    else if(calcType === 'metres2feet'){
+    else if(calcType === 'metre2feet'){
         resultSelector.value = Math.round(Number(inputSelector.value)*3.28084* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'centimeter2foot'){
+        resultSelector.value = Math.round(Number(inputSelector.value)/30.48* 1000) / 1000;
         calcAnsText.innerText = userHelpText(resultSelector.value);
     }
     else if(calcType === 'lbs2kg'){
         resultSelector.value = Math.round(Number(inputSelector.value)*0.45359237* 10000) / 10000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'million2crore'){
+        resultSelector.value = Number(inputSelector.value)/10;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'meter2centimeter'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*100* 100) / 100;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'centimeter2meter'){
+        resultSelector.value = inputSelector.value/100;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'centimeter2millimeter'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*10* 100) / 100;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'centimeter2kilometer'){
+        resultSelector.value = inputSelector.value/100000* 1000 / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'inch2feet'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*0.08333* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'meter2millimeter'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*1000* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'meter2kilometer'){
+        resultSelector.value = Math.round(Number(inputSelector.value)/1000* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'meter2inch'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*39.37007874* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'kilometer2mile'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*0.6213711922* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'cubicfeet2cubicmeter'){
+        resultSelector.value = Math.round(Number(inputSelector.value)/35.315* 1000) / 1000;
+        calcAnsText.innerText = userHelpText(resultSelector.value);
+    }
+    else if(calcType === 'hour2minute'){
+        resultSelector.value = Math.round(Number(inputSelector.value)*60* 1000) / 1000;
         calcAnsText.innerText = userHelpText(resultSelector.value);
     }
 
