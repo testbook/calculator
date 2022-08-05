9ab9149e963deb9c8615ea621235c52d7338192b
sumantestbook <suman@testbook.com>
2022-08-05 12:58:02 +0530

    added Grams To Moles Calculator

---
+++ script/tb-calc-type-15.js
@@ -0,0 +1,78 @@
+const params = new URLSearchParams(window.location.search)
+const calcType = params.get('type');
+
+const calcEmptyBody = document.getElementById('cardBody');
+
+const errorAlertText = 'Should be minimum 1 character.';
+const errorAlertSelector = document.getElementById('errorAlert');
+
+const inputNumberFirst = document.getElementById('inputNumberFirst');
+const inputNumberSecond = document.getElementById('inputNumberSecond');
+const resultBox = document.getElementById('resultBox');
+const errorAlert = document.getElementById('errorAlert');
+
+
+
+window.onload = function() {
+
+    switch (calcType) {
+        case "grams-moles":
+            break;
+        default:
+            calcEmptyBody.innerHTML = `
+                <div class="card__empty">
+                    <h3>Wrong URl</h3>
+                </div>
+            `
+    }
+};
+
+
+function calculateFun() {
+    if (!inputNumberFirst.checkValidity() || inputNumberFirst.validity.patternMismatch) {
+        inputNumberFirst.parentElement.classList.add('has-error');
+    }
+    else if(!inputNumberSecond.checkValidity() || inputNumberSecond.validity.patternMismatch){
+        inputNumberSecond.parentElement.classList.add('has-error');
+        
+    }else{
+        let finalValue = inputNumberFirst.value / inputNumberSecond.value;
+         resultBox.value = finalValue;
+    }
+
+}
+
+function onkeyPressFun() {
+    inputNumberFirst.parentElement.classList.remove('has-error');
+    inputNumberSecond.parentElement.classList.remove('has-error');
+    errorAlertSelector.innerHTML = '';
+    checkEvent(event);
+}
+
+function checkEvent(event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+        event.preventDefault();
+    }
+     if (event.code === 'Space') {
+
+        if (inputNumberFirst.value.length == 0) {
+            errorAlertSelector.innerHTML = errorAlertText;
+            inputNumberFirst.parentElement.classList.add('has-error');
+        }
+
+        if(inputNumberFirst.value.length !== 0 && inputNumberSecond.value.length == 0){
+            errorAlertSelector.innerHTML = errorAlertText;
+            inputNumberSecond.parentElement.classList.add('has-error');
+        }
+
+    }
+}
+
+function resetFun() {
+    inputNumberFirst.parentElement.classList.remove('has-error');
+    inputNumberSecond.parentElement.classList.remove('has-error');
+    inputNumberFirst.value = "";
+    inputNumberSecond.value = "";
+    errorAlertSelector.innerHTML = "";
+    resultBox.value = "";
+}
