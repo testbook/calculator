93603f10f7796a10b9d7479bbd971e6ca1dd590a
sumantestbook <suman@testbook.com>
2022-08-25 13:06:36 +0530

    added new file

---
+++ script/tb-calc-type-19.js
@@ -0,0 +1,76 @@
+const params = new URLSearchParams(window.location.search)
+const calcType = params.get('type');
+
+const calcEmptyBody = document.getElementById('cardBody');
+
+const errorAlertText = 'Should be minimum 1 character.';
+const errorAlertSelector = document.getElementById('errorAlert');
+
+const inputNumber = document.getElementById('inputNumber');
+const resultBoxFirst = document.getElementById('resultBoxFirst');
+const resultBoxSecond = document.getElementById('resultBoxSecond');
+const errorAlert = document.getElementById('errorAlert');
+
+
+
+window.onload = function() {
+
+    switch (calcType) {
+        case "variance-calculator":
+            break;
+        default:
+            calcEmptyBody.innerHTML = `
+                <div class="card__empty">
+                    <h3>Wrong URl</h3>
+                </div>
+            `
+    }
+};
+
+
+function calculateFun() {
+    if (!inputNumber.checkValidity() || inputNumber.validity.patternMismatch) {
+        
+        inputNumber.parentElement.classList.add('has-error');
+
+    }else{
+
+        let finalValue = math.fraction(inputNumber.value);
+        console.log(finalValue);
+        
+        resultBoxFirst.value = finalValue.n;
+        resultBoxSecond.value = finalValue.d;
+    }
+
+}
+
+
+
+function onkeyPressFun() {
+    inputNumber.parentElement.classList.remove('has-error');
+    errorAlertSelector.innerHTML = '';
+    checkEvent(event);
+}
+
+function checkEvent(event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+        event.preventDefault();
+    }
+    if (event.code === 'Space') {
+
+        if (inputNumber.value.length == 0) {
+            errorAlertSelector.innerHTML = errorAlertText;
+            inputNumber.parentElement.classList.add('has-error');
+        }
+        
+
+    }
+}
+
+function resetFun() {
+    inputNumber.parentElement.classList.remove('has-error');
+    inputNumber.value = "";
+    errorAlertSelector.innerHTML = "";
+    resultBoxFirst.value = "";
+    resultBoxSecond.value = "";
+}
