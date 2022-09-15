ebdbe4a1561286239d6fe072ab7c540732293f3b
Sumant-Testbook <sumant.dusane@testbook.com>
2022-09-15 17:48:32 +0530

    fibonacci series

---
+++ script/tb-calc-type-27.js
@@ -0,0 +1,85 @@
+const params = new URLSearchParams(window.location.search)
+const calcType = params.get('type');
+
+const calcEmptyBody = document.getElementById('cardBody');
+
+const errorAlertText = 'Should be minimum 1 character.';
+const errorAlertSelector = document.getElementById('errorAlert');
+
+const inputNumberFirst = document.getElementById('inputNumberFirst');
+const inputNumberSecond = document.getElementById('inputNumberSecond');
+const resultBox = document.getElementById('resultBox');
+const errorAlert = document.getElementById('errorAlert');
+
+
+
+window.onload = function() {
+
+    switch (calcType) {
+        case "fibonacci-series-calculator":
+            break;
+        default:
+            calcEmptyBody.innerHTML = `
+                <div class="card__empty">
+                    <h3>Wrong URl</h3>
+                </div>
+            `
+    }
+};
+
+
+function calculateFun() {
+    if (!inputNumberFirst.checkValidity()) {
+        inputNumberFirst.parentElement.classList.add('has-error');
+    }else if (!inputNumberSecond.checkValidity()){
+        inputNumberSecond.parentElement.classList.add('has-error');
+    }
+    else{
+        a = inputNumberFirst.value;
+        b = inputNumberSecond.value;
+        let n1 = 0, n2 = 1, next;
+        arr = [];
+
+        for(let i = 1; i <= b; i++){
+            arr.push(n1);
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+        
+        resultBox.value = String(arr.slice(a,b));
+    }
+
+}
+
+function onkeyPressFun() {
+    inputNumberFirst.parentElement.classList.remove('has-error');
+    inputNumberSecond.parentElement.classList.remove('has-error');
+    errorAlertSelector.innerHTML = '';
+    checkEvent(event);
+}
+
+function checkEvent(event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+        event.preventDefault();
+    }
+    if (event.code === 'Space') {
+
+        if (inputNumberFirst.value.length == 0) {
+            errorAlertSelector.innerHTML = errorAlertText;
+            inputNumberFirst.parentElement.classList.add('has-error');
+        }else if (inputNumberSecond.value.length == 0) {
+            errorAlertSelector.innerHTML = errorAlertText;
+            inputNumberSecond.parentElement.classList.add('has-error');
+        }
+    }
+}
+
+function resetFun() {
+    inputNumberFirst.parentElement.classList.remove('has-error');
+    inputNumberFirst.value = "";
+    inputNumberSecond.parentElement.classList.remove('has-error');
+    inputNumberSecond.value = "";
+    errorAlertSelector.innerHTML = "";
+    resultBox.value = "";
+}
